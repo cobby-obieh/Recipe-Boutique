2b47c7b0229375c0118cff7d1c38541519cb3bcd
cobby-obieh <cobbyobieh@gmail.com>
2023-03-24 23:57:32 +0000

    Recipe-Boutique

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div>
+        <p>Recipe Boutique©2023</p> 
+      </div>
+    );
+  }
+  export default Footer;
+  
---
+++ src/recipe.js
@@ -0,0 +1,40 @@
+export const my_recipe = {
+  recipe_1: {
+    name: 'Chirashi Don',
+    preparation_time: '30 minutes',
+    servings: '2',
+    ingredients: [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    instructions: [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+ 
+  },
+ 
+};
+
+export const my_recipe2 = {
+  recipe_1: {
+    name: ' Gari Soaking',
+    preparation_time: '3 minutes',
+    servings: '1',
+    ingredients: [
+      '1/2 cup gari',
+      '1/4 cup water',
+      '1/2 cup of milk',
+      'A table spoon of sugar',
+    ],
+    instructions: [
+      'Get a 1/2 cup of Gari',
+      'Add 1/2 cup of milf and table spoon of sugar',
+      'Slowly add water and eat quickly before it cakes',
+    ],
+ 
+  }
+};
